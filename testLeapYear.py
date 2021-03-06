952639de2347aa51c91a44611943a93b433eb01d
Kevin Pfeil <kevpfeil25@gmail.com>
2021-03-06 13:27:20 -0800

    added third test, checks for the year 100

--- testLeapYear.py
+++ testLeapYear.py
@@ -6,6 +6,8 @@ class TestCase(unittest.TestCase):
         self.assertEqual(leapyear.leapyear(400), True)
     def test2(self):
         self.assertEqual(leapyear.leapyear(4), True)
+    def test3(self):
+        self.assertEqual(leapyear.leapyear(100), False)
 
 if __name__ == '__main__':
     unittest.main()
