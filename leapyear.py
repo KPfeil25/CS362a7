af7dd76a6d27a70ce26a3bca1094e35c26680604
Kevin Pfeil <kevpfeil25@gmail.com>
2021-03-06 13:24:36 -0800

    wrote second test, tests for the year 4

--- leapyear.py
+++ leapyear.py
@@ -1,3 +1,5 @@
 def leapyear(num):
     if num % 400 == 0:
         return True
+    if num % 4 == 0:
+        return True
--- testLeapYear.py
+++ testLeapYear.py
@@ -3,7 +3,9 @@ import leapyear
 
 class TestCase(unittest.TestCase):
     def test1(self):
-        self.assertAlmostEqual(leapyear.leapyear(400), True)
-            
+        self.assertEqual(leapyear.leapyear(400), True)
+    def test2(self):
+        self.assertEqual(leapyear.leapyear(4), True)
+
 if __name__ == '__main__':
     unittest.main()
