43080652f77bd72051cbe39c8bdc79dd5cd3dd63
Kevin Pfeil <kevpfeil25@gmail.com>
2021-03-06 13:13:13 -0800

    implemented fizzbuzz method, solves the fizzbuzz problem

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,15 @@
 def fizzbuzz():
-    pass
+    res = ""
+    for num in range(1, 101):
+        if (num % 15 == 0):
+            res += "FizzBuzz"
+        elif (num % 5 == 0):
+            res += "Buzz"
+        elif (num % 3 == 0):
+            res += "Fizz"
+        else:
+            res += str(num)
+    #print(res)
+    return res
+
+fizzbuzz()
